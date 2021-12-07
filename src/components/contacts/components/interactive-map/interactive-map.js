6861d120f5e8df21a7122aace4647b06e8efa68d
Анастасия <kamanina963@gmail.com>
2021-12-07 07:40:14 +0300

    Для карты настроен пользовательский маркер

--- src/components/contacts/components/interactive-map/interactive-map.js
+++ src/components/contacts/components/interactive-map/interactive-map.js
@@ -1,28 +1,36 @@
-import { MapSetting } from 'const';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import { MapSetting, PinSize } from 'const/map';
+import pointer from 'assets/img/icon-location.svg';
 import * as S from './interactive-map.styled';
 
-const InteractiveMap = ({onLoaded}) => (
+const InteractiveMap = ({onLoaded}) => {
+  const pointerIcon = new L.Icon({
+    iconUrl: pointer,
+    iconRetinaUrl: pointer,
+    iconSize: PinSize.Icons,
+    iconAnchor: PinSize.Anchors,
+  });
+
+  return (
   <S.MapWrapper>
     <MapContainer
       center={MapSetting.Position}
       zoom={MapSetting.Zoom}
       scrollWheelZoom={false}
-      whenReady={() => {
-        onLoaded(true)
-      }}
+      whenReady={() => onLoaded(true)}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={MapSetting.Position}>
+      <Marker position={MapSetting.Position} icon={pointerIcon}>
         <Popup>
           мы находимся по адресу Санкт-Петербург, Набережная реки Карповка, д 5
         </Popup>
       </Marker>
     </MapContainer>
   </S.MapWrapper>
-);
+)};
 
 export default InteractiveMap;
---
+++ src/const/map.js
@@ -0,0 +1,14 @@
+const MapSetting = {
+  Position: [59.968137, 30.316272],
+  Zoom: 17,
+};
+
+const PinSize = {
+  Anchors: [25, 63],
+  Icons: [50, 63],
+}
+
+export {
+  MapSetting,
+  PinSize
+}
